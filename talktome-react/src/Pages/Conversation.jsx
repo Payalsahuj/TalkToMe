77f8a10c5f78bb10891038abe188f26059618082
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-04-14 12:52:00 +0530

    Correction

--- talktome-react/src/Pages/Conversation.jsx
+++ talktome-react/src/Pages/Conversation.jsx
@@ -1,28 +1,35 @@
-import '../App.css';
-import axios from 'axios';
-import { useEffect, useRef, useState, useCallback } from 'react'; // Import useCallback
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import startimg from "../image/loadprop.png"
-import { useNavigate } from 'react-router-dom';
+import "../App.css";
+import axios from "axios";
+import { useEffect, useRef, useState, useCallback } from "react"; // Import useCallback
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import startimg from "../image/loadprop.png";
+import { useNavigate } from "react-router-dom";
 
 function Conversation() {
-  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
   const [start, setstart] = useState(false);
-  const [isSpeaking, setIsSpeaking] = useState(false); 
-  const [isUserClicked, setIsUserClicked] = useState(false); 
-  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-  const [inputval, setinputval] = useState( '');
-  const [speak, setspeak] = useState('');
-  const navigate=useNavigate()
-  
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [isUserClicked, setIsUserClicked] = useState(false);
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  const [inputval, setinputval] = useState("");
+  const [speak, setspeak] = useState("");
+  const navigate = useNavigate();
 
-  const defaultVoice = window.speechSynthesis.getVoices().find(voice => voice.default);
+  const defaultVoice = window.speechSynthesis
+    .getVoices()
+    .find((voice) => voice.default);
 
   let speech = new SpeechSynthesisUtterance();
 
   useEffect(() => {
     window.speechSynthesis.onvoiceschanged = () => {
-      const defaultVoice = window.speechSynthesis.getVoices().find(voice => voice.default);
+      const defaultVoice = window.speechSynthesis
+        .getVoices()
+        .find((voice) => voice.default);
       speech.voice = defaultVoice;
     };
   }, []);
@@ -34,18 +41,15 @@ function Conversation() {
 
     speech.onstart = () => {
       setIsSpeaking(true);
-    }
-      
+    };
+
     speech.onend = () => {
       setIsSpeaking(false);
       if (isUserClicked) {
-       
-          startListening();
-          setstart(true);
-        
+        startListening();
+        setstart(true);
       }
-    }
-    
+    };
 
     window.speechSynthesis.speak(speech);
   }, [speak, defaultVoice, isUserClicked]);
@@ -53,8 +57,8 @@ function Conversation() {
   const debounceTimerRef = useRef(null);
 
   const fetchdata = useCallback(() => {
-    if (inputval === '') {
-      console.log('Need to communicate');
+    if (inputval === "") {
+      console.log("Need to communicate");
     } else {
       SpeechRecognition.stopListening();
       setstart(false);
@@ -62,7 +66,7 @@ function Conversation() {
         value: inputval,
       };
       axios
-        .post('https://talktome-ujhx.onrender.com/talktome', data)
+        .post("https://talktome-ujhx.onrender.com/talktome", data)
         .then((res) => {
           console.log(res.data);
           setspeak(res.data);
@@ -73,10 +77,22 @@ function Conversation() {
 
   useEffect(() => {
     setinputval(transcript);
+    const handleSpeechEnd = () => {
+      const finalTranscript = transcript.toLowerCase().trim();
+
+      if (
+        finalTranscript.includes("stop") ||
+        finalTranscript.includes("stop this conversation")
+      ) {
+        stoplist();
+      } else {
+        fetchdata(); // Only fetch if not saying stop
+      }
+    };
     clearTimeout(debounceTimerRef.current);
     debounceTimerRef.current = setTimeout(() => {
-      fetchdata();
-    }, 3500);
+      handleSpeechEnd();
+    }, 2500);
 
     return () => {
       clearTimeout(debounceTimerRef.current);
@@ -85,45 +101,68 @@ function Conversation() {
 
   function sayany() {
     setstart(true);
-    setinputval('');
+    setinputval("");
     setIsUserClicked(true);
     startListening();
   }
   function stoplist() {
-    window.speechSynthesis.cancel(); // Cancel speech synthesis
+    window.speechSynthesis.cancel();
     setstart(false);
     setIsUserClicked(false);
-    setinputval('');
-    setspeak(''); // Clear the speak state to prevent the last response from speaking again
+    setinputval("");
+    setspeak("");
     SpeechRecognition.stopListening();
-    navigate("/thank")
+    navigate("/thank");
   }
-  
-  
 
   if (!browserSupportsSpeechRecognition) {
     return null;
   }
 
   return (
-    <div style={{ backgroundColor: 'rgba(21,25,31,255)', height: '100vh', padding: '2%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} className="App">
-      <h1 style={{ color: 'white', fontFamily: 'monospace', fontSize: '35px' }}><b>𝕿𝖆𝖑𝖐 𝖙𝖔 𝖒𝖊</b></h1>
-      {start ? <img className='imageone' src="https://cdn.dribbble.com/users/121337/screenshots/1309485/loading.gif" alt="" /> : <img className='imagetwo' src={startimg} alt="" />}
+    <div
+      style={{
+        backgroundColor: "#15191F",
+        height: "100vh",
+        padding: "2%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      className="App"
+    >
+      <h1 className="animated-title">𝕿𝖆𝖑𝖐 𝖙𝖔 𝖒𝖊</h1>
 
-      <input style={{ visibility: 'hidden' }} value={inputval} onChange={(e) => setinputval(e.target.value)}></input>
+      {start ? (
+        <img
+          className="image imageone"
+          src="https://cdn.dribbble.com/users/121337/screenshots/1309485/loading.gif"
+          alt="loading"
+        />
+      ) : (
+        <img className="image imagetwo" src={startimg} alt="start" />
+      )}
+
+      <input
+        style={{ visibility: "hidden" }}
+        value={inputval}
+        onChange={(e) => setinputval(e.target.value)}
+      />
 
       <div className="btn-style">
-        <button className='button' style={{ color: 'white', backgroundColor: 'grey', fontFamily: 'monospace', width: '150px', height: '40px' }} onClick={sayany} disabled={isSpeaking}><b>Start Conversation</b></button>
-        <button className='button' style={{ color: 'white', backgroundColor: 'grey', fontFamily: 'monospace', width: '150px', height: '40px' }} onClick={stoplist}><b>Stop Conversation</b></button>
+        <button className="button" onClick={sayany} disabled={isSpeaking}>
+          <b>Start Conversation</b>
+        </button>
+        <button className="button" onClick={stoplist}>
+          <b>Stop Conversation</b>
+        </button>
       </div>
     </div>
   );
 }
 
 export default Conversation;
-
-
-
 
 // https://media3.giphy.com/media/QzdJer4CUPGheUFa1n/giphy.gif
 // https://farm5.staticflickr.com/4876/39891228293_13c532f352_o.gif
--- talktome-react/src/Pages/Welcome.jsx
+++ talktome-react/src/Pages/Welcome.jsx
@@ -1,17 +1,38 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Welcome=()=>{
-    const navigate=useNavigate()
-    useEffect(()=>{
-        setTimeout(()=>{
-            navigate("/conversation")
-        },4000)
-       
-    },[])
-    return (<div style={{height:'100vh'}}>
-    <div style={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'center',backgroundColor:'black',width:'100%',height:'100%'}}>
-      <img src='https://media2.giphy.com/media/jUJtNKKVTdIeD0XreN/giphy.gif?cid=790b7611242031e216d1fa871e586bcb2bbd0e3e2e14b8fd&rid=giphy.gif&ct=s' alt=''/>
+export const Welcome = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/conversation");
+    }, 4000);
+  }, []);
+  return (
+    <div style={{ height: "100vh" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "black",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <span
+          className="zoom-animation"
+          style={{
+            color: "white",
+            fontFamily: "monospace",
+            fontSize: "30px",
+            animation: "zoomInOut 4s ease-in-out infinite",
+          }}
+        >
+          Welcome to Talk to Me
+        </span>
+      </div>
     </div>
-    </div>)
-}
+  );
+};
